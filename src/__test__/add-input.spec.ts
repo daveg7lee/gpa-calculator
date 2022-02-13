30a3e84ac015fa8d054ddd9e52627d4324ff1699
Dave <daveg7lee@gmail.com>
2022-02-13 17:04:27 +0900

    Jest Testing!

---
+++ src/__test__/add-input.spec.ts
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+import { render, cleanup } from '@testing-library/svelte';
+import AddInput from '../components/AddInput.svelte';
+
+afterEach(() => {
+  cleanup();
+});
+
+test('should render one input & one button', () => {
+  const { getByRole } = render(AddInput, {
+    inputs: [{ subject: 'History', score: 99 }],
+  });
+
+  const button = getByRole('button');
+  const input = getByRole('textbox');
+
+  expect(button).toHaveTextContent('Add');
+  expect(input).toHaveAttribute('placeholder', 'Add new Subject you want');
+});
+
+test('should');
